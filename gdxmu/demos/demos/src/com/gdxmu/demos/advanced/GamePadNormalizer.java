d36dcc807303c66999e8dec0f77ac3e4018a6919
Terry Walsh <terrywalsh86@rocketmail.com>
2014-01-06 23:17:10 +1000

    Fix gdxmu advanced demo comments

--- gdxmu/demos/demos/src/com/gdxmu/demos/advanced/GamePadNormalizer.java
+++ gdxmu/demos/demos/src/com/gdxmu/demos/advanced/GamePadNormalizer.java
@@ -1,7 +1,7 @@
 package com.gdxmu.demos.advanced;
 
 // NOTE: Implement to associate GamePadDescriptor codes with your game logic.
-// See com.gdxmu.gamepads.demos.Xbox360Normalizer for an example.
+// See com.gdxmu.demos.advanced.MyXbox360Normalizer for an example.
 public interface GamePadNormalizer {
 	int normalizeButton(int buttonCode);
 	int normalizeAxis(int axisCode, float value);
